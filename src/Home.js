9645d39306782c2eed3fcf96b444911d09372fff
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-03-07 01:19:52 +0530

    Basic done

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+const Home = () => {
+
+
+    return (
+        <div>
+            <p className="text-center" >Homepage</p>
+        </div>
+    )
+
+}
+
+export default Home;
