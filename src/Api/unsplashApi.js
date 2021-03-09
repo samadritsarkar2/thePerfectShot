f37e1afe967d2c6a5645ef45c72b1351a0307793
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-03-09 23:58:53 +0530

    api added

--- src/Api/unsplashApi.js
+++ src/Api/unsplashApi.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const endpoint = 'https://api.unsplash.com/topics/nature/photos'
-
+const task = () => {
+    //
+}
 const response = await axios.get(endpoint);
+
+
 
 
 
