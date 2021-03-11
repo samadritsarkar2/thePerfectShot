20038e21b2053fec4cd5c8183b396e993ca7934d
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-03-11 23:58:40 +0530

    api fetching

--- src/Api/unsplashApi.js
+++ src/Api/unsplashApi.js
@@ -5,10 +5,7 @@ const endpoint = 'https://api.unsplash.com/topics/nature/photos';
 export const fetchData = async () => {
     try {
         const response = await axios.get(endpoint);
-        // if(response)
-        // {
-
-        // }
+      
         return response;
     } catch (err) {
         return console.log(err);
