8c287843b5a8c2e340f979078129443e80057e55
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-03-18 23:55:14 +0530

    footer changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import react from "react";
 const Footer = () => {
     return (
         <div style={{ backgroundColor: 'whitesmoke' , position : "static", bottom : 0, textAlign : 'center'}} >
-            <p>Infinite Scroll by Sam :heart: </p>
+            <p>Infinite Scroll by Sam </p>
         </div>
     )
 }
