bfc9331603b23ccf2dee7f2d5df94758e22e23be
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-03-20 23:16:59 +0530

    1

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
         <div className="container-fluid mt-2">
             <Nav />
             <Heading />
-            {/* <p className="text-center" >Homepage</p> */}
+          
 
             <App  />
             <Footer />
