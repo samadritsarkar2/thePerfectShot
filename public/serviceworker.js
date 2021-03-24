507aaddbc883700d6da10e75f1e05c0ace546fa5
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-03-25 02:10:35 +0530

    sw try

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,15 @@
 var cache = 'unsplashGalleryApp';
 
-
-self.addEventListener('install', event => {
-    console.log(event);
-})
+self.addEventListener('install', evt => {
+    console.log('service worker installed');
+  });
+  
+  // activate event
+  self.addEventListener('activate', evt => {
+    console.log('service worker activated');
+  });
+  
+  // fetch event
+  self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+  });
